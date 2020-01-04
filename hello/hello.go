7290a1bd3c2c2a89ec4d16f948f2bd26dc703d24
JinseonKim <30405743+kjs0662@users.noreply.github.com>
2020-01-04 20:00:58 +0900

    updated

--- hello/hello.go
+++ hello/hello.go
@@ -1,89 +1,102 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"os"
+	"strings"
 )
 
-func get(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "get called"}`))
+//Response return type struct
+type Response struct {
+	Images []string
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	//GET displays the upload form.
+	case "GET":
+		files, err := ioutil.ReadDir("/Users/jinseonkim/go/src/hello/storage/")
+		if err != nil {
+			panic(err)
+		}
 
-	fmt.Println("File Upload Endpoint Hit")
+		var fileArray []string
+		for _, file := range files {
+			newfile := "/Users/jinseonkim/go/src/hello/storage/" + file.Name()
+			if err != nil {
+				panic(err)
+			}
+			if strings.HasSuffix(newfile, "png") {
+				fileArray = append(fileArray, newfile)
+			}
+		}
+		fmt.Print(fileArray)
+		response := Response{fileArray}
+		js, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	// Parse our multipart form, 10 << 20 specifies a maximum
-	// upload of 10 MB files.
-	r.ParseMultipartForm(10 << 20)
-	// FormFile returns the first file for the given key `myFile`
-	// it also returns the FileHeader so we can get the Filename,
-	// the Header and the size of the file
-	file, handler, err := r.FormFile("image")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		w.Write([]byte(`{"message": "post called error"}`))
-		return
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+
+	//POST takes the uploaded file(s) and saves it to disk.
+	case "POST":
+		//get the multipart reader for the request.
+		reader, err := r.MultipartReader()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			println("Error occur to read data")
+			return
+		}
+
+		//copy each part to destination.
+		for {
+			part, err := reader.NextPart()
+			if err == io.EOF {
+				break
+			}
+
+			//if part.FileName() is empty, skip this iteration.
+			if part.FileName() == "" {
+				continue
+			}
+
+			dst, err := os.Create("/Users/jinseonkim/go/src/hello/storage/" + part.FileName())
+			defer dst.Close()
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				println("Error occur to save data")
+				return
+			}
+
+			if _, err := io.Copy(dst, part); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				println("Error occur to copy data")
+				return
+			}
+		}
+		//display success message.
+		println("upload", "Upload successful.")
+		fmt.Fprint(w, "success")
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-	// Create a temporary file within our temp-images directory that follows
-	// a particular naming pattern
-	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
-	if err != nil {
-		fmt.Println(err)
-		w.Write([]byte(`{"message": "post called error2"}`))
-	}
-	defer tempFile.Close()
-
-	// read all of the contents of our uploaded file into a
-	// byte array
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// write this byte array to our temporary file
-	tempFile.Write(fileBytes)
-	// return that we have successfully uploaded our file!
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
-}
-
-func put(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte(`{"message": "put called"}`))
-}
-
-func delete(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "delete called"}`))
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte(`{"message": "not found"}`))
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", get).Methods(http.MethodGet)
-	r.HandleFunc("/", post).Methods(http.MethodPost)
-	r.HandleFunc("/", put).Methods(http.MethodPut)
-	r.HandleFunc("/", delete).Methods(http.MethodDelete)
-	r.HandleFunc("/", notFound)
-	log.Fatal(http.ListenAndServe(":9090", r))
+	http.HandleFunc("/", uploadHandler)
+
+	//static file handler.
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	//Listen on port 9090
+	http.ListenAndServe(":9090", nil)
 }
